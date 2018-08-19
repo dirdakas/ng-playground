5299c767eb37613feaebd616794db11972fa8d22
Lukas Dirda <lukas.dirda@gmail.com>
2018-08-19 19:45:16 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  isRectangleClicked: boolean;
+  isCircle: boolean;
+
+  isLoadingBarActive: boolean;
+
+  loadingBarClicked() {
+    this.isLoadingBarActive = true;
+
+    setTimeout(() => {
+      this.isLoadingBarActive = false;
+    }, 3000);
+  }
 }
