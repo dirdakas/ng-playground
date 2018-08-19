865516ce6596a5eca5d8793d146d1c907905f0c9
Lukas Dirda <lukas.dirda@gmail.com>
2018-08-19 21:26:30 +0200

    added orange figure with animations in controller

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,45 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('clickedState', [
+      state('default', style({
+        backgroundColor: 'orange',
+        width: '150px',
+        height: '150px'
+      })),
+      state('clicked', style({
+        backgroundColor: 'blue',
+        width: '300px',
+        height: '50px'
+      })),
+      state('mousedown', style({
+        backgroundColor: 'lime',
+        width: '150px',
+        height: '150px',
+        border: '2px solid blue'
+      })),
+      transition('default => clicked', animate(
+        '0.3s 0.5s ease-in'
+      )),
+      transition('clicked => default', animate(
+        '0.5s 0.3s ease-in'
+      )),
+      transition('mousedown <=> clicked', animate(
+        '1s 0.5s linear'
+      ))
+    ])
+  ]
 })
 export class AppComponent {
   isRectangleClicked: boolean;
   isCircle: boolean;
+
+  clickedOrange = 'default';
 
   isLoadingBarActive: boolean;
 
@@ -17,5 +49,23 @@ export class AppComponent {
     setTimeout(() => {
       this.isLoadingBarActive = false;
     }, 3000);
+  }
+
+  changeOrange() {
+    switch (this.clickedOrange) {
+      case 'default':
+        this.clickedOrange = 'clicked';
+        break;
+      case 'clicked':
+        this.clickedOrange = 'default';
+        break;
+      case 'mousedown':
+        this.clickedOrange = 'clicked';
+        break;
+    }
+
+    if (this.clickedOrange === 'clicked') {
+      setTimeout(() => { this.clickedOrange = 'default'; } , 3000);
+    }
   }
 }
