67bcc8fa29e10677a8cfcafe36746f8ab3565abc
Lukas Dirda <lukas.dirda@gmail.com>
2018-08-22 20:18:56 +0200

    move animations to seperate file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,64 +1,13 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { clickedStateTrigger, setNumberStateTrigger } from './animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   animations: [
-    trigger('clickedState', [
-      state('default', style({
-        backgroundColor: 'orange',
-        width: '150px',
-        height: '150px'
-      })),
-      state('clicked', style({
-        backgroundColor: 'blue',
-        width: '300px',
-        height: '50px'
-      })),
-      state('mousedown', style({
-        backgroundColor: 'lime',
-        width: '150px',
-        height: '150px',
-        border: '2px solid blue'
-      })),
-      transition('default => clicked', animate(
-        '0.3s 0.5s ease-in'
-      )),
-      transition('clicked => default', animate(
-        '0.5s 0.3s ease-in'
-      )),
-      transition('mousedown <=> clicked', animate(
-        '1s 0.5s linear'
-      ))
-    ]),
-    trigger('numberState', [
-      state('notSelected', style({
-        color: 'grey',
-        paddingLeft: '5px',
-        paddingRight: '5px',
-        border: '1px solid black'
-      })),
-      state('selected', style({
-        color: 'red',
-        backgroundColor: 'yellow',
-        paddingLeft: '5px',
-        paddingRight: '5px',
-        border: '1px solid lime'
-      })),
-      transition('notSelected => selected', [
-        style({
-          border: '2px solid black',
-          padding: '4px'
-        }),
-        animate(400, style({
-          backgroundColor: 'red',
-          padding: '6px'
-        })),
-        animate(700)
-      ])
-    ])
+    clickedStateTrigger,
+    setNumberStateTrigger
   ]
 })
 export class AppComponent {
