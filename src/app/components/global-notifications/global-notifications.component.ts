2d5826db6e819c28c22122ebae4d547960c8a249
Lukas Dirda <lukas.dirda@gmail.com>
2019-09-03 08:30:36 +0300

    magic global error notification fix

--- src/app/components/global-notifications/global-notifications.component.ts
+++ src/app/components/global-notifications/global-notifications.component.ts
@@ -1,24 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, OnDestroy } from '@angular/core';
+
+import { Subscription } from 'rxjs';
 
 import { GlobalNotificationsService } from '../../services/global-notifications/global-notifications.service';
 import { IGlobalNotification } from '../../interfaces/global-notification';
-
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-global-notifications',
   templateUrl: './global-notifications.component.html',
   styleUrls: ['./global-notifications.component.scss']
 })
-export class GlobalNotificationsComponent implements OnInit {
-  notifications$: Observable<IGlobalNotification[]>;
+export class GlobalNotificationsComponent implements OnInit, OnDestroy {
+  notificationsSub: Subscription;
+  notifications: IGlobalNotification[] = [];
 
   constructor(
-    private globalNotificationsService: GlobalNotificationsService
+    private globalNotificationsService: GlobalNotificationsService,
+    private cd: ChangeDetectorRef
   ) {}
 
   ngOnInit(): void {
-    this.notifications$ = this.globalNotificationsService.notifications$
-      .pipe();
+    this.notificationsSub = this.globalNotificationsService.notifications$
+      .pipe()
+      .subscribe((notifications: IGlobalNotification[]) => {
+        this.notifications = notifications;
+        this.cd.detectChanges();
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.notificationsSub.unsubscribe();
   }
 }
